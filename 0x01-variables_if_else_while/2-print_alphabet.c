797581eb94d7791e7934764c7b456395540cf2ba
Taoheed <oyeniyiemperor@gmail.com>
2022-06-03 22:21:01 +0100

    putchar

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<time.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main()
+{
+char alp;
+char nl = 012;
+for (alp = 'a' ; alp <= 'z' ; alp++)
+{
+putchar(alp);
+}
+putchar(nl);
+return (0);
+}
+
