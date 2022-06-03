c6339db4b044239f17a220408f004113972ab02d
Taoheed <oyeniyiemperor@gmail.com>
2022-06-03 22:47:19 +0100

    alphabet - (e && q)

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<time.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+char alp;
+char nl;
+nl = 012;
+for (alp = 'a' ; alp <= 'z' ; alp++)
+{
+  if (alp != 'e' && alp != 'q')
+{
+putchar(alp); 
+}
+}
+putchar(nl);
+return (0);
+}
