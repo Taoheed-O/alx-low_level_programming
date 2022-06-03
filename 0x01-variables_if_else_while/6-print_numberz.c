a12bafa8463dfdab48f9d0f8e010d73050ee95a9
Taoheed <oyeniyiemperor@gmail.com>
2022-06-03 23:31:00 +0100

    numberz putchar

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<time.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+char num;
+char nl;
+nl = 012;
+for (num = 48 ; num < 58 ; num++)
+{
+putchar(num);
+}
+putchar(nl);
+return (0);
+}
