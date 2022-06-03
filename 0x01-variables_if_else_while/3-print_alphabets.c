4ae897c48be24b0012673559bc1d7fb961361f15
Taoheed <oyeniyiemperor@gmail.com>
2022-06-03 22:33:50 +0100

    upper and lower cases

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<time.h>
+/**
+ *
+ *
+ *
+ */
+int main(void)
+{
+char alp;
+char alph;
+char n;
+n = 012;
+for (alp = 'a' ; alp <= 'z' ; alp++)
+{
+putchar(alp);
+}
+for (alph = 'A' ; alph <= 'Z' ; alph++)
+{
+putchar(alph);
+}
+putchar(n);
+return (0);
+}
