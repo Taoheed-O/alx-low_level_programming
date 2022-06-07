d5595362ca3d1a1dda63f9ef8fada6a93209a8ff
Taoheed <oyeniyiemperor@gmail.com>
2022-06-07 20:56:14 +0100

    _putchar prototype

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,4 +1,4 @@
-#include<stdio.h>
+#include"main.h"
 /**
 * main - Entry point
 *
@@ -6,6 +6,13 @@
 */
 int main(void)
 {
-printf("_putchar\n");
+int str[] = {45, 112, 117, 116, 99, 104, 97, 114};
+int count, sz;
+sz = sizeof(str) / sizeof(int);
+for (count = 0; count < sz; count++)
+{
+_putchar(str[count]);
+}
+_putchar('\n');
 return (0);
 }
