0b69804cd745c1a5554cab603ff04874b1039f2b
Taoheed <oyeniyiemperor@gmail.com>
2022-06-07 22:33:17 +0100

    print sign

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+* print_sign - Sign of integers
+*
+* @n: n is the variable integer
+*
+* Return: 0 if n is 0. 1 if n is + and -1 if n is -
+*/
+int print_sign(int n)
+{
+if (n > 0)
+{
+return (1);
+}
+else if (n == 0)
+{
+return (0);
+}
+else
+{
+return (-1);
+}
+}
