e9936e23d08edfd26a6474a35cb027222376d46d
Taoheed <oyeniyiemperor@gmail.com>
2022-06-07 22:22:25 +0100

    all alphabets checker

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,12 +1,14 @@
 #include "main.h"
 /**
-* _isalpha: A main function that checks for alphabet
+* _isalpha - A main function that checks for alphabet
 *
 * @c: a parameter that can take any value/data type
+*
+* Return: 1 for letters and 0 for non-alphabet
 */
 int _isalpha(int c)
 {
-  if ((c >= 97 && c <= 122) || (c >= 65 && c <= 90))
+if ((c >= 97 && c <= 122) || (c >= 65 && c <= 90))
 {
 return (1);
 }
