1c9d4c83dc7f2cc8b5d266f0bc1b47adb9fe17f4
Taoheed <oyeniyiemperor@gmail.com>
2022-06-09 20:16:19 +0100

    digit printer

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+* print_numbers - Prints the integers 0-9
+*
+* Return: Always 0 (Success)
+*/
+void print_numbers(void)
+{
+int num;
+for (num = 48 ; num < 58 ; num++)
+{
+_putchar(num);
+}
+_putchar('\n');
+}
