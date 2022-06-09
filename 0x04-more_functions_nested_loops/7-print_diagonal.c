c24aeb92aa96dfb932906525eba40b1892d4d5f6
Taoheed <oyeniyiemperor@gmail.com>
2022-06-10 00:54:43 +0100

    print _ diagonally

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+* print_diagonal - diagonally
+*@n: the parameter
+* Return: Always 0 Success
+*/
+void print_diagonal(int n)
+{
+int s;
+int i;
+if (n <= 48)
+{
+_putchar('\n');
+}
+for (s = 1 ; s <= n  ; s++)
+{
+for (i = 1; i < s; i++)
+{
+_putchar(' ');
+}
+_putchar('\\');
+_putchar('\n');
+}
+}
