da0b09a8fde8dbdecb9284451fda9a8f8f9a0936
Taoheed <oyeniyiemperor@gmail.com>
2022-06-09 23:46:38 +0100

    most numbers

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+* print_most_numbers - print numbers from 0-9 except 2 and 4
+*
+* Return: Always 0 (Success)
+*/
+void print_most_numbers(void)
+{
+int num;
+for (num = 48 ; num < 58 ; num++)
+{
+if (num != 50 && num != 52)
+{
+_putchar(num);
+}
+}
+_putchar('\n');
+}
