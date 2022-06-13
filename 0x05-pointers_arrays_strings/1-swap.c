4734986a4c0efc8592c3de3abcb52b344a6146d3
Taoheed <oyeniyiemperor@gmail.com>
2022-06-13 17:16:57 +0100

    Swap Deal

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+* swap_int - swapping the values of the two parameters
+* @a: the first integer
+* @b: the second integer
+* Return: Always 0 (Success)
+*/
+void swap_int(int *a, int *b)
+{
+*b = *a;
+}
